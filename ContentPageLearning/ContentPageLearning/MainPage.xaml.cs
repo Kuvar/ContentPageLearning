3f0799f84e4a3c491cb9971cbfdfcff2a0ec8024
Kuvar Singh <kuvarjava@gmail.com>
2018-03-06 23:34:53 +0530

    ContentPageLearning
    ContentPageLearning

---
+++ ContentPageLearning/ContentPageLearning/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ContentPageLearning
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            SubmitButton.Clicked += SubmitButton_Clicked;
+
+        }
+
+        private void SubmitButton_Clicked(object sender, EventArgs e)
+        {
+            LblMessage.Text = "Hi " + txtName.Text;
+            LblMessage.FontSize = 24;
+            LblMessage.TextColor = Color.Blue;
+        }
+    }
+}
